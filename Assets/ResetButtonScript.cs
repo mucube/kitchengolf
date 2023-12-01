18a287aca9a16c53ce8be0e74b1993e6fe29faa7
Aaron <mucube0@gmail.com>
2023-12-01 13:01:54 -0700

    reset button

--- Assets/ResetButtonScript.cs
+++ Assets/ResetButtonScript.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResetButtonScript : MonoBehaviour
 {
+    public Rigidbody2D ball;
+    public Button resetButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Button btn = resetButton.GetComponent<Button>();
+		btn.onClick.AddListener(ResetBall);
     }
 
+    void ResetBall(){
+		ball.transform.position = new Vector2(0,0);
+        ball.velocity = new Vector2(0,0);
+	}
     // Update is called once per frame
     void Update()
     {
